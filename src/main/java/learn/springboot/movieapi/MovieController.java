fae1d319af741456e8cc108bf654e76edeab0825
excaliburonly <saumya20-269@aec.ac.in>
2024-08-04 21:40:49 +0530

    CORS added

--- src/main/java/learn/springboot/movieapi/MovieController.java
+++ src/main/java/learn/springboot/movieapi/MovieController.java
@@ -1,6 +1,5 @@
 package learn.springboot.movieapi;
 
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
